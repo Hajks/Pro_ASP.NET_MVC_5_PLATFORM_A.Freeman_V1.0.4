b6a9ff60b734616bc2189f728c3634489534e180
Hajk <tunczorx@gmail.com>
2018-11-14 12:38:36 +0100

    Add project files.

---
+++ Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1.0.4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1._0._4.MvcApplication" Language="C#" %>
---
+++ Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1.0.4/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1._0._4
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
